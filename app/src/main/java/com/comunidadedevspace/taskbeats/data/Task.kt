3465a95fa07782d4c808d110c0f6749a2eb150ac
Aline Ferreira <alinfst@gmail.com>
2024-02-28 05:36:33 -0300

    create package layers

--- app/src/main/java/com/comunidadedevspace/taskbeats/data/Task.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/data/Task.kt
@@ -1,4 +1,4 @@
-package com.comunidadedevspace.taskbeats
+package com.comunidadedevspace.taskbeats.data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
--- app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/presentation/TaskDetailActivity.kt
@@ -1,4 +1,4 @@
-package com.comunidadedevspace.taskbeats
+package com.comunidadedevspace.taskbeats.presentation
 
 import android.app.Activity
 import android.content.Context
@@ -11,6 +11,8 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.comunidadedevspace.taskbeats.R
+import com.comunidadedevspace.taskbeats.data.Task
 import com.google.android.material.snackbar.Snackbar
 
 class TaskDetailActivity : AppCompatActivity() {
